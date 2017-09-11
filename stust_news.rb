98919d6974694e81cc8e27af8d725ae3e363fbf7
titangene <titangene01@gmail.com>
2017-09-11 12:50:05 +0800

    Add single news page, Fix news page, Refactor

--- stust_news.rb
+++ stust_news.rb
@@ -17,13 +17,14 @@ Capybara.register_driver :poltergeist do |app|
   Capybara::Poltergeist::Driver.new(app, options)
 end
 
+newsBaseURL = "http://news.stust.edu.tw/id/"
 url = "http://news.stust.edu.tw/User/UserShowNewsList.aspx"
 visit(url)
 
 news_table_css = "table#ctl00_ContentPlaceHolder1_gv_NewsList "
 news_table_tr_td_css = "tr:not(:first-child):not(:last-child) td:nth-child(2)"
 news_table_date_css = "tr:not(:first-child):not(:last-child) td:last-child"
-news_title_css = "span#ctl00_ContentPlaceHolder1_FormView1_TitleLabel"
+news_title_css = "h4.flow-text span"
 
 # 過濾個人不需要的公告關鍵字，可自訂
 @filters = ["約聘", "助理", "工讀生", "計畫徵求", "計畫申覆", "自主培育", "招標", "二技申請入學", 
@@ -57,11 +58,11 @@ _page = ARGV[0]
 _page ||= 1
 _page = _page.to_i
 
-debug_mode = ARGV[1]
+mode = ARGV[1]
 
 if _page == 1
   puts "value 1: Please input the number of pages printed (optional)"
-  puts "value 2: input 'd' (optional) --> Debug mode (filter)"
+  puts "value 2: mode (optional)，'d' -> Debug mode (filter)，'s' -> Single news"
 end
 
 @filter_bool = true
@@ -95,56 +96,79 @@ end
 
 i, j = 0, 0
 
-while i < _page do
-  news_list = all(news_table_css + news_table_tr_td_css)
-  news_date_list = all(news_table_css + news_table_date_css)
-  news_list_count = news_list.count
-  puts "------------------------- 第 #{i + 1} 頁 -------------------------"
-  
-  while j < news_list_count do
-    _sleep(1, 16, news_table_css)
+if mode == 's'
+  news_url = "#{newsBaseURL}#{_page}"
+  visit(news_url)
+  _sleep(1, 16, news_title_css)
+  newsHTML = Nokogiri::HTML(page.body)
+  news_title = newsHTML.css(news_title_css).text
+  news_date = newsHTML.css("span#ctl00_ContentPlaceHolder1_lbl_date").text
+  news_deptname = newsHTML.css("span#ctl00_ContentPlaceHolder1_lbl_deptname").text
+  news_content = newsHTML.css("span#ctl00_ContentPlaceHolder1_lbl_content").inner_html.gsub(/(<br>)+/, "\n")
+  news_attachments = newsHTML.css("h6.flow-text a.black-text")
+  puts news_url
+  puts news_title
+  puts news_date
+  puts "發佈單位：" + news_deptname
+  puts "------------------------------------------"
+  puts news_content
+  puts "------------------------------------------"
+  news_attachments.each do |attachment|
+    news_attachment_text = attachment.text
+    news_attachment_url = attachment.values[1]
+    puts "#{news_attachment_text}  #{news_attachment_url}"
+  end
+else
+  while i < _page do
+    news_list = all(news_table_css + news_table_tr_td_css)
+    news_date_list = all(news_table_css + news_table_date_css)
+    news_list_count = news_list.count
+    puts "------------------------- 第 #{i + 1} 頁 -------------------------"
+    
+    while j < news_list_count do
+      _sleep(1, 16, news_table_css)
 
-    news = news_list[j].find('div.list_menu a')
-    news_title = news[:title]
-    news_url = news[:href]
-    news_date = news_date_list[j].text
-    _filter(news_title)
-
-    if @filter_save_bool
+      news = news_list[j].find('div.list_menu a')
+      news_title = news[:title]
+      news_url = news[:href]
+      news_date = news_date_list[j].text
       news_no = j + 1 + (i * news_list_count)
       news_no = "%03d" % news_no
-      if debug_mode == 'd'
-        puts "No.#{news_no}: #{news_date} #{news_url} '#{@filter_save_str}': #{news_title}"
-      else
-        puts "No.#{news_no}: #{news_date} #{news_url} : #{news_title}"
-      end
-    elsif !@filter_bool and debug_mode == 'd'
-      puts "Filter: #{news_date} #{news_url} '#{@filter_str}': #{news_title}"
-    elsif @filter_bool
-      news_list[j].find('div.list_menu a').trigger('click')
-      _sleep(1, 16, news_title_css)
-      newsHTML = Nokogiri::HTML(page.body)
-      news_title = newsHTML.css(news_title_css).text
+      news_puts_text = "No.#{news_no}: #{news_date} #{news_url}"
+      _filter(news_title)
 
-      if @filter_bool
-        news_no = j + 1 + (i * news_list_count)
-        news_no = "%03d" % news_no
-        if debug_mode == 'd'
-          puts "No.#{news_no}: #{news_date} #{news_url} '#{@filter_save_str}': #{news_title}"
+      if @filter_save_bool
+        if mode == 'd'   # debug mode
+          puts "#{news_puts_text} '#{@filter_save_str}': #{news_title}"
         else
-          puts "No.#{news_no}: #{news_date} #{news_url} : #{news_title}"
+          puts "#{news_puts_text} : #{news_title}"
         end
-      elsif !@filter_bool and debug_mode == 'd'
+      elsif !@filter_bool and mode == 'd'   # debug mode
         puts "Filter: #{news_date} #{news_url} '#{@filter_str}': #{news_title}"
-      end
+      elsif @filter_bool
+        news_list[j].find('div.list_menu a').trigger('click')
+        _sleep(1, 16, news_title_css)
+        newsHTML = Nokogiri::HTML(page.body)
+        news_title = newsHTML.css(news_title_css).text
 
-      page.go_back
-      news_list = all(news_table_css + news_table_tr_td_css)
-      news_date_list = all(news_table_css + news_table_date_css)
+        if @filter_bool
+          if mode == 'd'   # debug mode
+            puts "#{news_puts_text} '#{@filter_save_str}': #{news_title}"
+          else
+            puts "#{news_puts_text} : #{news_title}"
+          end
+        elsif !@filter_bool and mode == 'd'   # debug mode
+          puts "Filter: #{news_date} #{news_url} '#{@filter_str}': #{news_title}"
+        end
+
+        page.go_back
+        news_list = all(news_table_css + news_table_tr_td_css)
+        news_date_list = all(news_table_css + news_table_date_css)
+      end
+      j += 1
     end
-    j += 1
+    j = 0
+    i += 1
+    visit("http://news.stust.edu.tw/User/UserShowNewsList.aspx?page=#{i}")
   end
-  j = 0
-  i += 1
-  visit("http://news.stust.edu.tw/User/UserShowNewsList.aspx?page=#{i}")
 end
